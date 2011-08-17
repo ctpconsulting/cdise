caed4f298a54c2c32afdf16bfa7f3889fd5d8274
Sylvain Berthouzoz <sberthouzoz@gmail.com>
2011-08-17 18:04:40 +0200

    CDISE-6: list command implemented (it's dynamic, I ask the beanManager)

--- archiver/archiver-main/src/main/java/com/ctp/javaone/archiver/plugin/List.java
+++ archiver/archiver-main/src/main/java/com/ctp/javaone/archiver/plugin/List.java
@@ -1,22 +1,41 @@
 package com.ctp.javaone.archiver.plugin;
 
+import java.lang.annotation.Annotation;
+import java.util.Set;
+
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Any;
+import javax.enterprise.inject.Instance;
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.util.AnnotationLiteral;
+import javax.inject.Inject;
 
 import com.ctp.javaone.archiver.command.Command;
-import com.ctp.javaone.archiver.persistence.Auditable;
 
 @Command("list")
 @ApplicationScoped
-@Auditable
 public class List implements Plugin {
+    
+    @Inject 
+    private BeanManager beanManager;
 
+    @SuppressWarnings("serial")
     public String executeCommand(String... params) {
         StringBuilder greeting = new StringBuilder();
-        greeting.append("Available Archiver Commands:\n\n");
-        greeting.append("list\n");
-        greeting.append("archive\n");
-        greeting.append("audit\n");
-        greeting.append("exit\n");
+        Set<Bean<?>> plugins = beanManager.getBeans(Plugin.class, new AnnotationLiteral<Any>() {});
+        
+        greeting.append("Available Archiver Commands:\n");
+        for (Bean<?> bean : plugins) {
+            Set<Annotation> qualifiers = bean.getQualifiers();
+            for (Annotation annotation : qualifiers) {
+                if (annotation instanceof Command){
+                    greeting.append("\n");
+                    Command command = (Command) annotation;
+                    greeting.append(command.value());
+                }
+            }
+        }
         return greeting.toString();
     }
 
